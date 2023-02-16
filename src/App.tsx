1b7163e4ebebed9079a13024446791dacbe32c60
Yuric888 <111116745+Yuric888@users.noreply.github.com>
2023-02-16 16:07:51 +0700

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
-import { Container } from "react-bootstrap";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
 import Store from "./pages/Store/Store";
+import { Container } from "react-bootstrap";
+const NavbarWrapper = () => {
+  return (
+    <div className="pb-5">
+      <Navbar />
+      <Container>
+        <Outlet />
+      </Container>
+    </div>
+  );
+};
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/store",
-    element: <Store />,
-  },
-  {
-    path: "/about",
-    element: <About />,
+    element: <NavbarWrapper />,
+    children: [
+      {
+        path: "/", // yes, again
+        element: <Home />,
+      },
+      {
+        path: "/store",
+        element: <Store />,
+      },
+
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 function App() {
   return (
-    <Container>
+    <>
       <RouterProvider router={router} />
-    </Container>
+    </>
   );
 }
 

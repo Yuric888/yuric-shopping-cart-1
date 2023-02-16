e2bc581bb0e035df5c86c6c0a6657b5ef4359530
Yuric888 <111116745+Yuric888@users.noreply.github.com>
2023-02-16 10:42:47 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Container } from "react-bootstrap";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Store from "./pages/Store/Store";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/store",
+    element: <Store />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <RouterProvider router={router} />
+    </Container>
   );
 }
 
